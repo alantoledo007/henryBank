1f0728c41d531b0421b9342bd259c51dab94d3ce
alantoledo007 <alantoledo007@gmail.com>
2020-10-01 20:53:53 -0300

    Moleculer cli & mongo installation, and user model

---
+++ api/services/users.service.js
@@ -0,0 +1,103 @@
+"use strict";
+const DbMixin = require("../mixins/db.mixin");
+const DbService = require("moleculer-db");
+const MongooseAdapter = require("moleculer-db-adapter-mongoose");
+
+const User = require("../models/User");
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "users",
+	adapter: new MongooseAdapter("mongodb://localhost/moleculer-demo"),
+	mixins: [DbMixin("products")],
+	model:User,
+
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		hello: {
+			rest: {
+				method: "GET",
+				path: "/hello"
+			},
+			async handler() {
+				return "Hello Moleculer";
+			}
+		},
+
+		/**
+		 * Welcome, a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			rest: {
+                method: "GET",
+                path: "/welcome", //tambien puede ser /welcome/:name
+            },
+			params: {
+				name: "string"
+			},
+			/** @param {Context} ctx  */
+			async handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
