15870952f889a3245efac1907668dd885166c00f
Nicolas <nicokenny98@gmail.com>
2020-10-04 01:34:55 -0300

    S9 - Alta de usuario

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,8 @@
 const register = require('./auth/register');
+const client_registration = require('./auth/client_registration')
 
 module.exports = {
-    register
+    register,
+    client_registration,
+    
 }
