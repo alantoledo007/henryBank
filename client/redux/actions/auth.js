a1cb3140e320e28447238905081b716fb0587415
Francisco <franciscozapi@gmail.com>
2020-10-05 17:08:42 -0300

    Login: AsyncStorage

--- client/redux/actions/auth.js
+++ client/redux/actions/auth.js
@@ -1,6 +1,12 @@
-const LOGIN = 'LOGIN';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const LOGIN = 'LOGIN';
 
 export const login = data => {
+    //Cargamos estos datos primero a AsyncStorage
+    AsyncStorage.setItem('auth', JSON.stringify(data), err => {
+        if(err) console.log('ERROR en AsyncStorage.setItem en redux/actions/auth, en login',err);
+    });
     return {
         type: LOGIN,
         payload: data
