ddbcaedb614f52e11cf2bd424031de60d8871ec6
Ignacio Piris <ignaciopirisb@gmail.com>
2020-10-14 12:31:12 -0300

    recharge-service

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -1,28 +1,25 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-
-    const Transaction = sequelize.define(
-        
-        "Transaction",
-        {
-            transaction_id: {
-                type: DataTypes.UUID,
-                primaryKey: true,
-				defaultValue: Sequelize.UUIDV4,
-            },
-            title: {
-                type: DataTypes.STRING(255),
-            },
-            description: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-            },
-            amount: {
-                type: DataTypes.FLOAT(12,2),
-                allowNull: false,
-            }
-        }
-    )
-
-}
+	const Transaction = sequelize.define("Transaction", {
+		transaction_id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: Sequelize.UUIDV4,
+		},
+		title: {
+			type: DataTypes.STRING(255),
+		},
+		description: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+		},
+		amount: {
+			type: DataTypes.FLOAT(12, 2),
+			allowNull: false,
+		},
+		type: {
+			type: DataTypes.ENUM(["ingreso", "egreso"]),
+		},
+	});
+};
