bd48200046b159516455dba9953a64ce2481790d
Ignacio Piris <ignaciopirisb@gmail.com>
2020-10-14 12:38:48 -0300

    route protection

--- api/config.js
+++ api/config.js
@@ -4,8 +4,10 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 module.exports = {
 	db_uri: `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 
-	actions_protected: [ //todas las acciones incorporadas dentro de este array requerirán autenticación jwt
-		'auth.register_confirmation',
-		'transactions.transaction'
-	]
+	actions_protected: [
+		//todas las acciones incorporadas dentro de este array requerirán autenticación jwt
+		"auth.register_confirmation",
+		"transactions.transaction",
+		"recharge.recharge",
+	],
 };
