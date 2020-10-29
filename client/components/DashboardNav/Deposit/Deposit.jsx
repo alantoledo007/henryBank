913a1b9a890c7a076c12b1f905f4a35c1addd07c
Francisco <franciscozapi@gmail.com>
2020-10-29 09:35:54 -0300

    antes de la demo

--- client/components/DashboardNav/Deposit/Deposit.jsx
+++ client/components/DashboardNav/Deposit/Deposit.jsx
@@ -36,7 +36,7 @@ const Deposit = ({ closeModal, navigation }) => {
   }
 
   return (
-    <Container style={styles.container}  wihtHeader={true}>
+    <Container style={styles.container}  >
       {/* <LinearGradient
         // Background Linear Gradient
         colors={["rgba(0,0,0,0.8)", "#6b538a"]}
