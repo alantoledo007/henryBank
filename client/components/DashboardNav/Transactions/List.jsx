1c31a1fe57d31c48f961e97b5d6731f3001faf55
Nicolas <nvelandia97@gmail.com>
2020-10-30 13:12:38 -0300

    cambio de tarjeta

--- client/components/DashboardNav/Transactions/List.jsx
+++ client/components/DashboardNav/Transactions/List.jsx
@@ -10,8 +10,6 @@ import moment from 'moment';
 
 export default function List({ isFetching, transactions, select }) {
 
-  console.log(transactions)
-
   const theme = useColorScheme();
 
   const format = (amount) => {
